6d766dbab5890db0cab8cfcc39188a217fe6f780
Luke Martin <lukemartinwebdev@gmail.com>
2015-01-03 17:36:55 -0600

    add gravity constant, down arrow linked to right arrow

--- js/gameplay.js
+++ js/gameplay.js
@@ -1,17 +1,36 @@
 //Initialize the game area
 Crafty.init(500,350, document.getElementById('game'));
 
+//console.log(Crafty.timer.stepType('fixed'));
 //Add user entity
-Crafty.e('Man, 2D, DOM, Color, Fourway, Gravity')
+Crafty.e('Man, 2D, DOM, Color, Multiway, Gravity')
     .attr({x: 0, y: 0, w: 50, h: 50})
     .color('#A7C5BD')
-    .fourway(4) //<-- control entity with arrow keys
-    .gravity('Floor');
+    .multiway(4, {UP_ARROW: -90, DOWN_ARROW: 0, RIGHT_ARROW: 0, LEFT_ARROW: -180}) //<-- control entity with arrow keys
+    .gravity('Floor')
+    .gravityConst(0.2);
 
 //Add Floor(barrier) entity
 Crafty.e('Floor, 2D, DOM, Color')
     .attr({x: 0, y: 250, w: 250, h: 10})
     .color('#524656');
 Crafty.e('Floor, 2D, DOM, Color')
-    .attr({x: 320, y: 250, w: 180, h: 10})
+    .attr({x: 330, y: 250, w: 170, h: 10})
     .color('#524656');
+Crafty.e('Floor, 2D, DOM, Color')
+    .attr({x: 530, y: 250, w: 250, h: 10})
+    .color('#524656');
+Crafty.e('Floor, 2D, DOM, Color')
+    .attr({x: 800, y: 250, w: 250, h: 10})
+    .color('#524656');
+Crafty.e('Floor, 2D, DOM, Color')
+    .attr({x: 1100, y: 250, w: 170, h: 10})
+    .color('#524656');
+Crafty.e('Floor, 2D, DOM, Color')
+    .attr({x: 1300, y: 250, w: 250, h: 10})
+    .color('#524656');
+
+window.setInterval( function() {
+	moveRight(10) }, 
+	100
+);
