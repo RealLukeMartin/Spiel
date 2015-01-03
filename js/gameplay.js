48b6be750f941adf8b14c908182511995cf9620d
Luke Martin <lukemartinwebdev@gmail.com>
2015-01-03 14:00:13 -0600

    Add game stage and initial entity

---
+++ js/gameplay.js
@@ -0,0 +1,8 @@
+//Initialize the game area
+Crafty.init(500,350, document.getElementById('game'));
+
+//Add an entity
+Crafty.e('2D, DOM, Color').attr({x: 0, y: 0, w: 100, h: 100}).color('#f00');
+
+
+
