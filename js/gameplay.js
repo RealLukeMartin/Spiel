d6b18ec43cee914de18374ca5405f622de8bb537
Luke Martin <lukemartinwebdev@gmail.com>
2015-01-03 14:35:56 -0600

    add floor entity and gravity

--- js/gameplay.js
+++ js/gameplay.js
@@ -1,11 +1,16 @@
 //Initialize the game area
 Crafty.init(500,350, document.getElementById('game'));
 
-//Add an entity
-Crafty.e('2D, DOM, Color, Fourway')
-    .attr({x: 0, y: 0, w: 100, h: 100})
-    .color('#f00')
-    .fourway(4); //<-- control entity with arrow keys
+//Add Floor(barrier) entity
+Crafty.e('Floor, 2D, DOM, Color')
+    .attr({x: 0, y: 250, w: 250, h: 10})
+    .color('green');
 
+//Add user entity
+Crafty.e('2D, DOM, Color, Fourway, Gravity')
+    .attr({x: 0, y: 0, w: 50, h: 50})
+    .color('#f00')
+    .fourway(4) //<-- control entity with arrow keys
+    .gravity('Floor');
 
 
