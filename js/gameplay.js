1ce288c2f5825c263bf06793c652861c1b8e3bd0
Luke Martin <lukemartinwebdev@gmail.com>
2015-01-03 14:08:35 -0600

    add entity position control via arrow keys

--- js/gameplay.js
+++ js/gameplay.js
@@ -2,7 +2,10 @@
 Crafty.init(500,350, document.getElementById('game'));
 
 //Add an entity
-Crafty.e('2D, DOM, Color').attr({x: 0, y: 0, w: 100, h: 100}).color('#f00');
+Crafty.e('2D, DOM, Color, Fourway')
+    .attr({x: 0, y: 0, w: 100, h: 100})
+    .color('#f00')
+    .fourway(4); //<-- control entity with arrow keys
 
 
 
