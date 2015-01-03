61f2fb7073b0c497039c6c3e59e16f1fc58acf82
Luke Martin <lukemartinwebdev@gmail.com>
2015-01-03 14:51:08 -0600

    add another floor entity

--- js/gameplay.js
+++ js/gameplay.js
@@ -1,16 +1,17 @@
 //Initialize the game area
 Crafty.init(500,350, document.getElementById('game'));
 
-//Add Floor(barrier) entity
-Crafty.e('Floor, 2D, DOM, Color')
-    .attr({x: 0, y: 250, w: 250, h: 10})
-    .color('green');
-
 //Add user entity
-Crafty.e('2D, DOM, Color, Fourway, Gravity')
+Crafty.e('Man, 2D, DOM, Color, Fourway, Gravity')
     .attr({x: 0, y: 0, w: 50, h: 50})
-    .color('#f00')
+    .color('#A7C5BD')
     .fourway(4) //<-- control entity with arrow keys
     .gravity('Floor');
 
-
+//Add Floor(barrier) entity
+Crafty.e('Floor, 2D, DOM, Color')
+    .attr({x: 0, y: 250, w: 250, h: 10})
+    .color('#524656');
+Crafty.e('Floor, 2D, DOM, Color')
+    .attr({x: 320, y: 250, w: 180, h: 10})
+    .color('#524656');
