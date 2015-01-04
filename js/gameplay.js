fdb8035ff54860025e8181332ae649348dc64f15
Luke Martin <lukemartinwebdev@gmail.com>
2015-01-03 19:06:07 -0600

    add variable for user entity, remove scroll interval

--- js/gameplay.js
+++ js/gameplay.js
@@ -3,7 +3,7 @@ Crafty.init(500,350, document.getElementById('game'));
 
 //console.log(Crafty.timer.stepType('fixed'));
 //Add user entity
-Crafty.e('Man, 2D, DOM, Color, Multiway, Gravity')
+var man = Crafty.e('Man, 2D, DOM, Color, Multiway, Gravity')
     .attr({x: 0, y: 0, w: 50, h: 50})
     .color('#A7C5BD')
     .multiway(4, {UP_ARROW: -90, DOWN_ARROW: 0, RIGHT_ARROW: 0, LEFT_ARROW: -180}) //<-- control entity with arrow keys
@@ -30,7 +30,4 @@ Crafty.e('Floor, 2D, DOM, Color')
     .attr({x: 1300, y: 250, w: 250, h: 10})
     .color('#524656');
 
-window.setInterval( function() {
-	moveRight(10) }, 
-	100
-);
+
